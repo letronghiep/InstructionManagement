53a8b7612feac49354f2ef733845c7318225e3ad
letronghiep <letronghiep213@gmail.com>
2023-09-24 18:07:50 +0700

    Add project files.

---
+++ InstructionManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InstructionManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
